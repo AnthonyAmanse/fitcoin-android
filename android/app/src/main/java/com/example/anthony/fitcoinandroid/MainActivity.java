23729a763bcf9f176930fb5dbe851ebcf5f271f3
AnthonyAmanse <ghieamanse@gmail.com>
2018-04-07 15:55:18 -0700

    Initial android project
    Signed-off-by: AnthonyAmanse <ghieamanse@gmail.com>

---
+++ android/app/src/main/java/com/example/anthony/fitcoinandroid/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.anthony.fitcoinandroid;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_technology:
+                    mTextMessage.setText(R.string.title_technology);
+                    return true;
+                case R.id.navigation_user:
+                    mTextMessage.setText(R.string.title_user);
+                    return true;
+                case R.id.navigation_shop:
+                    mTextMessage.setText(R.string.title_shop);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
