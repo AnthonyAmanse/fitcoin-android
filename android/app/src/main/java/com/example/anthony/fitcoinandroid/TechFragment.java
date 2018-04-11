9a7e52cd0ce7cfb7a10d174c98f0fb010a4886ea
AnthonyAmanse <ghieamanse@gmail.com>
2018-04-11 11:24:35 -0700

    remove broken code on tech fragment
    Signed-off-by: AnthonyAmanse <ghieamanse@gmail.com>

--- android/app/src/main/java/com/example/anthony/fitcoinandroid/TechFragment.java
+++ android/app/src/main/java/com/example/anthony/fitcoinandroid/TechFragment.java
@@ -24,7 +24,7 @@ public class TechFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        ((AppCompatActivity) getActivity()).getSupportActionBar().hide();
+//        ((AppCompatActivity) getActivity()).getSupportActionBar().hide();
         return inflater.inflate(R.layout.fragment_tech, container, false);
     }
 
